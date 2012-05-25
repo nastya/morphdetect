58a15b0a3c34e44ba253481eb5e952b61183b6e8
Анастасия Щербинина <nastya_jane@mail.ru>
2012-05-25 16:24:44 +0400

    Frontend: split data in blocks before processing (when running on files).

--- main.cpp
+++ main.cpp
@@ -6,6 +6,10 @@
 #include <string>
 #include <cstring>
 #include <stdlib.h>
+#include <algorithm>
+
+#define BLOCK_SIZE 1500u
+#define OVERLAP 100u
 
 using namespace std;
 
@@ -33,10 +37,26 @@ int main(int argc, char** argv)
 	TimerAnalyzer::start(TimeLoad);
 	Reader reader;
 	reader.load(argv[1]);
-	ds.link(reader.pointer(), reader.size());
 	TimerAnalyzer::stop(TimeLoad);
 
+	/*
+	ds.link(reader.pointer(), reader.size());
 	string ans = ds.analyze();
+	*/
+	string ans;
+	for (int data_start = 0, i = 0; data_start < reader.size(); data_start += BLOCK_SIZE - OVERLAP, i++)
+	{
+		if (i % 10)
+			cout << 100 * (float) data_start / reader.size() << "% processed" << endl;
+
+		ds.link(reader.pointer() + data_start, min(reader.size() - data_start, BLOCK_SIZE));
+		string my_ans = ds.analyze();
+		if (!my_ans.empty()) {
+			ans = my_ans;
+			break;
+		}
+	}
+
 	if (!ans.empty())
 	{
 		cout << ans << endl;
@@ -45,6 +65,7 @@ int main(int argc, char** argv)
 	{
 		cout << "Not an attack" << endl;
 	}
+
 	TimerAnalyzer::stop(TimeTotal);
 	cerr << "Total time: " << TimerAnalyzer::secs(TimeTotal) << endl;
 	cerr << "Time spent on loading shellcodes: " << TimerAnalyzer::secs(TimeLoadShellcodes) << endl;
