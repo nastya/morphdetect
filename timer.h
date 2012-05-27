096ad1cab999c6995e46658da6e8b9b4d6b5b4e5
Анастасия Щербинина <nastya_jane@mail.ru>
2012-05-27 05:59:41 +0400

    Make timer functions inline.

--- timer.h
+++ timer.h
@@ -20,12 +20,29 @@ Calculate time
 
 class TimerAnalyzer {
 public:
+	static inline void start(TimeAnalyzerIds id = TimeTotal)
+	{
+		if (!enabled) return;
+		data[id] -= microtime();
+	}
+	static inline void stop(TimeAnalyzerIds id = TimeTotal)
+	{
+		if (!enabled) return;
+		data[id] += microtime();
+	}
+	static inline float secs(TimeAnalyzerIds id = TimeTotal)
+	{
+		return data[id] * 1e-6;
+	}
+	static inline long unsigned int microtime()
+	{
+		struct timeval tv;
+		gettimeofday(&tv,NULL);
+		return (long unsigned int) (1e6*tv.tv_sec + tv.tv_usec);
+	}
+
 	static bool enabled;
-	static void start(TimeAnalyzerIds id = TimeTotal);
-	static void stop(TimeAnalyzerIds id = TimeTotal);
-	static float secs(TimeAnalyzerIds id = TimeTotal);
 	static int data[TimeNone];
-	static long unsigned int microtime();
 };
 
 #endif //TIMER_ANALYZER_H
